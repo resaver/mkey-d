91f464d91003fb3f1a814d0aec476787f750a263
Resaver <75112689+resaver@users.noreply.github.com>
2022-10-10 23:31:18 +0700

    Add files via upload

---
+++ sweetalert.js
@@ -0,0 +1,18 @@
+async function ubahProfile() {
+    const { value: formValues } = await Swal.fire({
+        title: 'Multiple inputs',
+        html:
+          '<input id="swal-input1" class="swal2-input" placeholder="Nama">' +
+          '<input id="swal-input2" class="swal2-input" placeholder="Umur">',
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById('swal-input1').value,
+            document.getElementById('swal-input2').value
+          ]
+        }
+    })
+    if (formValues) {
+        Swal.fire(JSON.stringify(formValues))
+    }
+}
